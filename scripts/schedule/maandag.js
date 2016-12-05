9abde18fcf1f6da50f69adbc406f186bf1390b97
rickvdwerf <rick.tien@hotmail.com>
2016-12-05 13:00:34 +0100

    Added Schedule
    - School Schedule
    - different files for everyday of the week to put in all the
    things i have to do
    - other small fixes

---
+++ scripts/schedule/maandag.js
@@ -0,0 +1,19 @@
+    // maandag van 8 to 24 uur 
+
+    var ma8 = "Dingen die ik om 8 uur doe maandag.";
+    var ma9 = "Dingen die ik om 9 uur doe maandag.";
+    var ma10 = "Dingen die ik om 10 uur doe maandag.";
+    var ma11 = "Dingen die ik om 11 uur doe maandag.";
+    var ma12 = "Dingen die ik om 12 uur doe maandag.";
+    var ma13 = "Dingen die ik om 13 uur doe maandag.";
+    var ma14 = "Dingen die ik om 14 uur doe maandag.";
+    var ma15 = "Dingen die ik om 15 uur doe maandag.";
+    var ma16 = "Dingen die ik om 16 uur doe maandag.";
+    var ma17 = "Dingen die ik om 17 uur doe maandag.";
+    var ma18 = "Dingen die ik om 18 uur doe maandag.";
+    var ma19 = "Dingen die ik om 19 uur doe maandag.";
+    var ma20 = "Dingen die ik om 20 uur doe maandag.";
+    var ma21 = "Dingen die ik om 21 uur doe maandag.";
+    var ma22 = "Dingen die ik om 22 uur doe maandag.";
+    var ma23 = "Dingen die ik om 23 uur doe maandag.";
+    var ma24 = "Dingen die ik om 24 uur doe maandag.";
